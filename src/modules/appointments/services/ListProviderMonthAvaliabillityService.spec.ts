767083975e3db81fe03fd016b0ce775e542ce3e8
Daniel <dgl.daniel@outlook.com.br>
2020-06-19 23:09:39 -0300

    Express rate limit

--- src/modules/appointments/services/ListProviderMonthAvaliabillityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvaliabillityService.spec.ts
@@ -6,7 +6,7 @@ import ListProviderMonthAvaliabillityService from './ListProviderMonthAvaliabill
 let listProviderMonthAvaliabillity: ListProviderMonthAvaliabillityService;
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 
-describe('ListProviderMonthAvaliabillity', () => {
+describe('ListProviderMonthAvailability', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
     listProviderMonthAvaliabillity = new ListProviderMonthAvaliabillityService(
@@ -14,42 +14,84 @@ describe('ListProviderMonthAvaliabillity', () => {
     );
   });
 
-  it('should be able to list the month availabillity from provider', async () => {
+  it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      user_id: 'user_id',
+      user_id: '123123',
       date: new Date(2020, 4, 20, 8, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      user_id: 'user_id',
+      user_id: '123123',
       date: new Date(2020, 4, 20, 9, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      user_id: 'user_id',
+      user_id: '123123',
       date: new Date(2020, 4, 20, 10, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      user_id: 'user_id',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
+      date: new Date(2020, 4, 20, 17, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: '123123',
       date: new Date(2020, 4, 21, 8, 0, 0),
     });
 
-    const availabillity = await listProviderMonthAvaliabillity.execute({
+    const availability = await listProviderMonthAvaliabillity.execute({
       provider_id: 'user',
       year: 2020,
       month: 5,
     });
 
-    expect(availabillity).toEqual(
+    expect(availability).toEqual(
       expect.arrayContaining([
         { day: 19, available: true },
         { day: 20, available: false },
-        { day: 21, available: false },
+        { day: 21, available: true },
         { day: 22, available: true },
       ]),
     );
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,12 +10,14 @@ import AppError from '@shared/errors/AppError';
 
 import uploadConfig from '@config/upload';
 import routes from './routes';
+import rateLimiter from './middlewares/RateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
